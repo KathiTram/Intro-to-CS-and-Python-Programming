c73e3a7eec480d495de70a8d2458301bd39c429b
KathiTram <49622903+KathiTram@users.noreply.github.com>
2022-05-26 12:46:16 -0700

    Create 3.2FingerExercise.py

---
+++ Chapter3/3.2FingerExercise.py
@@ -0,0 +1,16 @@
+"""
+3.2 Finger Exercise
+Let s be a string that contains a sequence of decimal numbers
+separated by commas, e.g., s = '1.23,2.4,3.123'. Write a program that prints the
+sum of the numbers in s.
+"""
+
+s = '1.23,2.4,3.123'
+numbers = int()
+
+for num in s:
+    if num != ',' and num != '.':
+        numbers = numbers + int(num)
+    else:
+        continue
+print(numbers)
